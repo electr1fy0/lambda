60ce83c7beca8aba57bc9918fb81dafbe38cc2f2
electr1fy0 <electr1fy0@icloud.com>
2025-07-06 18:16:22 +0530

    templ test 2

--- api/hello.go
+++ api/hello.go
@@ -6,7 +6,8 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	component := views.Page("Hello from Templ", "This is rendered")
+	component := views.Page("Hello from Templ and is supposed to be tht title", "This is a rendered message")
+
 	w.Header().Set("Content-Type", "text/html")
 	component.Render(r.Context(), w)
 }
